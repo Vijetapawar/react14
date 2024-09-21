e36389117ff67c730a64a9228ed054bfc7d54e13
Vijeta Pawar <vijetapawar2003@gmail.com>
2024-09-21 15:55:31 +0530

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
-
+import About from "./components/About";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
 function App() {
-
+  let data = "hello this is data from the server";
+  let subject = [ "hindi", "english", "math"];
+  let profile = {
+    name: "John Doe",
+    age: 30,
+    city: "New York"
+  };
   return (
       <div>
-        <h1>hello</h1>
+        <Home/>
+        <About/>
+        <Nav/>
+        <h1>{data}</h1>
+        <h1>{subject}</h1>
+        <h1>{profile.name}</h1>
+        <h1>{profile.age}</h1>
+        <h1>{profile.city}</h1>
       </div>  
 )}
 
-export default App
+export default App;
