4e9bcf9a123cb4a90b16c2e0a4b857ecf5035ec1
Vijeta Pawar <vijetapawar2003@gmail.com>
2024-09-28 11:08:10 +0530

    usesate

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
+// import React from 'react'
+
+// const App = () => {
+//   const submittHandler = (e) => {
+//     e.preventDefault();
+//     const user = {
+//       username : e.target.username.value,
+//       email : e.target.email.value,
+//     }
+    
+//     console.log(user);
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={submittHandler}>
+//         <input type="username" placeholder="username" name="username" />
+//         <input type="email" placeholder="email" name="email" />
+//         <button type="submit">Submit</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default App
+
 import React from 'react'
 import { useState } from 'react';
 
 const App = () => {
-  const [time, settime] = useState(new Date().toLocaleTimeString());
-  setInterval(()=>{
-    settime(new Date().toLocaleTimeString());
-  },1000);
+  const [title, settitle] = useState("hello ji");
+  const changeInfo = () => {
+    settitle("it's changed");    
+    console.log(title);
+  }
 
   return (
     <div>
-      <h1>{time}</h1>    
+      <h1>{title}</h1>
+      <button onClick={changeInfo}> click me!</button>
+
     </div>
   )
 }
