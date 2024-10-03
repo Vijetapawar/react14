9ceeb2e4742e94443da4bf42e9101cf530d5af0f
Vijeta Pawar <vijetapawar2003@gmail.com>
2024-10-03 21:06:37 +0530

    useform

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,46 @@
-// import React from 'react'
-
-// const App = () => {
-//   const submittHandler = (e) => {
-//     e.preventDefault();
-//     const user = {
-//       username : e.target.username.value,
-//       email : e.target.email.value,
-//     }
-    
-//     console.log(user);
-//   };
-//   return (
-//     <div>
-//       <form onSubmit={submittHandler}>
-//         <input type="username" placeholder="username" name="username" />
-//         <input type="email" placeholder="email" name="email" />
-//         <button type="submit">Submit</button>
-//       </form>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-// import React, { useState } from 'react'
-
-// const App = () => {
-   
-//   const [gender, setGender] = useState("others");
-
-//   return (
-//     <div>
-//       <input type="radio" defaultValue="others" />
-//       <input type="radio" 
-//       name="gender"
-//       checked={gender === "female" ? true:false}
-//       defaultChecked="female"
-//       onChange={(e)= ()=>{setGender(e.target.value)}}
-//       />
-//       Female
-//       <select>
-//         <option value="city">
-          
-//         </option>
-//       </select>              
-//     </div>
-//   )
-// }
-
-// export default App
-
-import React from 'react'
-import { useState } from 'react'
+import {useForm} from 'react-hook-form';
 
 const App = () => {
-  const [username, setUsername] = useState("");
-  const submitHandler = (e) =>{
-   e.preventDefault();
-  //  console.log(e.targer.username.value);
-  console.log({username});
-  
-   
+  const {register,handleSubmit, watch, formState:{errors}} = useForm();
+  const SubmitHandler = (data) => {
+    console.log(data);
+    
   }
-  console.log(username);
+  // console.log(watch("username"));
+  console.log(errors);
   
   return (
-    <div>
-     <h1>React Forms</h1>    
-     <form onSubmit={submitHandler}>
-      <input onChange={(e) => setUsername(e.target.value)} value={username} type="text" placeholder="username" name="username"/>
-      <button>Sbumit</button>
-     </form>
-    </div>
+    <>
+      <h1 className="text-3xl ">Register utls</h1>
+      <form onSubmit={handleSubmit(SubmitHandler)} className="p-5">
+        <input type="text" placeholder="username"{...register("username",{
+          required:"username field is required",
+          maxLength:8,
+          minLength:4
+        })} />
+      {errors.username?.type == "required" &&(
+        <p className="text-red-300 mt-3">
+        {errors.username.message}
+      </p>)}
+
+
+      {errors.username?.type == "maxlength" && (
+        <p className="text-red-300 mt-3">
+        {errors.username.message}
+      </p>)}
+
+        <input type="radio"
+        defaultValue= "male" 
+        {...register("gender")} />Male
+        <br />
+        <input type="radio"
+        defaultValue= "female"
+        {...register("gender")} />Female
+       <br />
+        <button>submit</button>
+      </form>
+    </>
   )
 }
 
 export default App
-
-
-
-
-
